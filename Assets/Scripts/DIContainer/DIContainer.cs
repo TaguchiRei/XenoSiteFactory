9bf3c69efc56bbeb96655650a494c7afff703435
TaguchiRei <ren.len2426@gmail.com>
2025-06-10 16:58:52 +0900

    [add]登録、解除、取得用メソッド完成

--- Assets/Scripts/DIContainer/DIContainer.cs
+++ Assets/Scripts/DIContainer/DIContainer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +8,8 @@ namespace DIContainer
     public class DiContainer : MonoBehaviour
     {
         [SerializeField] private Scene _managerScene;
+
+        private Dictionary<Type, object> _container;
 
         public static DiContainer Instance;
         private void Awake()
@@ -18,21 +22,32 @@ namespace DIContainer
             {
                 Destroy(gameObject);
             }
+            _container = new();
         }
 
         public void Register<T>(T instance)
         {
-            
+            _container.Add(typeof(T), instance);
         }
 
         public void Unregister<T>(T instance)
         {
-            
+            _container.Remove(typeof(T));
         }
 
         public bool TryGet<T>(out T instance)
         {
-            
+            if (_container.ContainsKey(typeof(T)))
+            {
+                instance = (T)_container[typeof(T)];
+                return true;
+            }
+            else
+            {
+                Debug.Log($"type {typeof(T)} not found");
+                instance = default;
+                return false;
+            }
         }
 
         private void Start()
