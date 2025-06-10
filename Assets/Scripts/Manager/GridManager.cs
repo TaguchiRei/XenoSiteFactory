3ec7c63215c48ad266354dad256403982b57e918
TaguchiRei <ren.len2426@gmail.com>
2025-06-10 18:40:02 +0900

    [add]ユニットデータの保存、占有座標の保存をできるように

--- Assets/Scripts/Manager/GridManager.cs
+++ Assets/Scripts/Manager/GridManager.cs
@@ -1,10 +1,47 @@
+using System.Collections.Generic;
+using Interface;
 using UnityEngine;
 
 namespace Manager
 {
-    public class GridManager : MonoBehaviour
+    public class GridManager : MonoBehaviour, IManager
     {
-        
+        [SerializeField, Range(20, 200)] private int gridSize = 20;
+
+        /// <summary>
+        /// グリッドが占有されているかどうかを示す情報
+        /// </summary>
+        public bool[,,] Grid { get; private set; }
+
+        /// <summary>
+        /// グリッドに設置されている物を保存する
+        /// </summary>
+        public List<PutUnitData> PutUnitData { get; private set; }
+
+        /// <summary>
+        /// 初期化時に呼び出される
+        /// </summary>
+        public void Initialize()
+        {
+            Grid = new bool[gridSize, gridSize, gridSize];
+            PutUnitData = new();
+        }
     }
-    
+
+    /// <summary>
+    /// 設置済みユニットのデータ
+    /// </summary>
+    public struct PutUnitData
+    {
+        public int UnitId;
+        public Vector2Int Position;
+        public Vector2Int Direction;
+    }
+
+    public struct UnitData
+    {
+        public ulong UnitShape;
+        public Vector2Int[] EnterPositions;
+        public Vector2Int[] ExitPositions;
+    }
 }
