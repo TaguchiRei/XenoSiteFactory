d341cc7392216d3c7d9453b262bd30e0b587253c
TaguchiRei <ren.len2426@gmail.com>
2025-06-10 21:40:33 +0900

    [update]２重配列だったものを3重配列に対応させる

--- Assets/Scripts/Manager/GridManager.cs
+++ Assets/Scripts/Manager/GridManager.cs
@@ -42,64 +42,114 @@ namespace Manager
             bool[,,] unitShape = new bool[4, 4, 4];
             foreach (var putUnitData in PutUnitDataList)
             {
-                Vector2Int basePos = putUnitData.Position;
-                Vector2Int baseDir = putUnitData.Direction;
                 UnitData unit = _allUnitData.AllUnit[(int)putUnitData.unitType][putUnitData.UnitId];
-                
-                for (int z = 1; z <= 4; z++)
+
+                //boolの配列を回転
+                var shape = GetUnitShape(unit.UnitShape);
+                switch (putUnitData.Direction)
                 {
-                    for (int y = 1; y <= 4; y++)
+                    case UnitRotate.Right90:
+                        shape = RotateRight90(shape);
+                        break;
+                    case UnitRotate.Right180:
+                        shape = RotateRight180(shape);
+                        break;
+                    case UnitRotate.Right270:
+                        shape = RotateRight270(shape);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ulongの値をboolの配列に変換
+        /// </summary>
+        /// <param name="shape">形を示すulongを入れる</param>
+        /// <returns></returns>
+        private bool[,,] GetUnitShape(ulong shape)
+        {
+            var unitShape = new bool[4, 4, 4];
+            for (int z = 1; z <= 4; z++)
+            {
+                for (int y = 1; y <= 4; y++)
+                {
+                    for (int x = 1; x <= 4; x++)
                     {
-                        for (int x = 1; x <= 4; x++)
+                        int bitPosition = (x - 1) + (y - 1) * 4 + (z - 1) * 16;
+                        if ((shape & ((ulong)1 << bitPosition)) != 0)
                         {
-                            int bitPosition = (x-1) + (y-1) * 4 + (z-1) * 16;
-                            if ((unit.UnitShape & ((ulong)1 << bitPosition)) != 0)
-                            {
-                                unitShape[x, y, z] = true;
-                            }
+                            unitShape[x, y, z] = true;
                         }
                     }
                 }
             }
+
+            return unitShape;
         }
-        public bool[,] RotateRight90(bool[,] matrix)
+
+        /// <summary>
+        /// boolの配列を90度右回転させる。
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public bool[,,] RotateRight90(bool[,,] matrix)
         {
-            bool[,] result = new bool[4, 4];
-            for (int i = 0; i < 4; i++)
+            bool[,,] result = new bool[4, 4, 4];
+            for (int z = 0; z < 4; z++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int x = 0; x < 4; x++)
                 {
-                    result[j, 3 - i] = matrix[i, j];
+                    for (int y = 0; y < 4; y++)
+                    {
+                        result[y, 3 - x, z] = matrix[x, y, z];
+                    }
                 }
             }
+
             return result;
         }
 
-        // 180度回転
-        public bool[,] RotateRight180(bool[,] matrix)
+        /// <summary>
+        ///　boolの配列を180度回転させる
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public bool[,,] RotateRight180(bool[,,] matrix)
         {
-            bool[,] result = new bool[4, 4];
-            for (int i = 0; i < 4; i++)
+            bool[,,] result = new bool[4, 4, 4];
+            for (int z = 0; z < 4; z++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int x = 0; x < 4; x++)
                 {
-                    result[3 - i, 3 - j] = matrix[i, j];
+                    for (int y = 0; y < 4; y++)
+                    {
+                        result[3 - x, 3 - y, z] = matrix[x, y, z];
+                    }
                 }
             }
+
             return result;
         }
 
-        // 270度右回転 (90度左回転と同じ)
-        public bool[,] RotateRight270(bool[,] matrix)
+        /// <summary>
+        /// boolの配列を270度回転させる
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public bool[,,] RotateRight270(bool[,,] matrix)
         {
-            bool[,] result = new bool[4, 4];
-            for (int i = 0; i < 4; i++)
+            bool[,,] result = new bool[4, 4, 4];
+            for (int z = 0; z < 4; z++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int x = 0; x < 4; x++)
                 {
-                    result[3 - j, i] = matrix[i, j];
+                    for (int y = 0; y < 4; y++)
+                    {
+                        result[3 - y, x, z] = matrix[x, y, z];
+                    }
                 }
             }
+
             return result;
         }
     }
@@ -114,9 +164,10 @@ namespace Manager
         /// スクリプタブルオブジェクト内の配列のインデックス番号と対応
         /// </summary>
         public int UnitId;
+
         public UnitType unitType;
         public Vector2Int Position;
-        public Vector2Int Direction;
+        public UnitRotate Direction;
     }
 
     [Serializable]
