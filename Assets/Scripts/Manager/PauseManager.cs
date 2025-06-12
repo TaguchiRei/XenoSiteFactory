929ffbac41b8063cab526b2c28fb9f57e9ff48ed
TaguchiRei <ren.len2426@gmail.com>
2025-06-12 10:06:50 +0900

    [add]Pause、Resumeメソッド作成

--- Assets/Scripts/Manager/PauseManager.cs
+++ Assets/Scripts/Manager/PauseManager.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using DIContainer;
 using Interface;
 using UnityEngine;
@@ -7,14 +7,31 @@ namespace Manager
 {
     public class PauseManager : MonoBehaviour, IManager
     {
+        
+        private List<IPauseable> Pausables = new();
+        
+        
         void IManager.Register()
         {
             DiContainer.Instance.Register<IManager>(this);
         }
+        
+        
 
-        void Pouse()
+        private void Pause()
         {
-            
+            foreach (var pauseable in Pausables)
+            {
+                pauseable.Pause();
+            }
+        }
+
+        private void Resume()
+        {
+            foreach (var pauseable in Pausables)
+            {
+                pauseable.Resume();
+            }
         }
     }
 }
