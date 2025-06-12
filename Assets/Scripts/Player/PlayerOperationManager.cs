988ef47b5d4c0cbedee4940cba2ce09ebe277880
TaguchiRei <ren.len2426@gmail.com>
2025-06-12 12:16:47 +0900

    [add]UI操作も追加、regionで囲う

--- Assets/Scripts/Player/PlayerOperationManager.cs
+++ Assets/Scripts/Player/PlayerOperationManager.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 
 namespace Player
 {
-    public class PlayerOperationManager : MonoBehaviour, IManager, InputSystem_Actions.IPlayerActions
+    public class PlayerOperationManager : MonoBehaviour, IManager, InputSystem_Actions.IPlayerActions,
+        InputSystem_Actions.IUIActions
     {
         private InputSystem_Actions inputSystemActions;
 
@@ -15,30 +16,60 @@ namespace Player
             DiContainer.Instance.Register(this);
         }
 
+        #region Player
         public void OnMove(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnInteract(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnPrevious(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnNext(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnMouseMove(InputAction.CallbackContext context)
         {
-            
         }
+
+        #endregion
+        
+        #region UI
+        public void OnNavigate(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSubmit(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCancel(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPoint(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnClick(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnRightClick(InputAction.CallbackContext context)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
 
         public void Register()
         {
