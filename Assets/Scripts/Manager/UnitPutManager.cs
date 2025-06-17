25fa4435407c6effe30d98af46ff3db64ffd1ce4
TaguchiRei <ren.len2426@gmail.com>
2025-06-17 18:19:28 +0900

    [add]レイヤーを一つ上に上げるスクリプト作成

---
+++ Assets/Scripts/Manager/UnitPutManager.cs
@@ -0,0 +1,37 @@
+using DIContainer;
+using Interface;
+using UnityEngine;
+
+namespace Manager
+{
+    public class UnitPutManager : MonoBehaviour, IManager
+    {
+        [SerializeField] private int layerLimit = 4;
+        public int PutLayer
+        {
+            get;
+            private set;
+        }
+
+
+        public void UpLayer()
+        {
+            PutLayer++;
+            if (PutLayer > layerLimit)
+            {
+                PutLayer = 1;
+            }
+        }
+        
+        
+        public void Register()
+        {
+            DiContainer.Instance.Register<IManager>(this);
+        }
+
+        public void Initialize()
+        {
+            Register();
+        }
+    }
+}
