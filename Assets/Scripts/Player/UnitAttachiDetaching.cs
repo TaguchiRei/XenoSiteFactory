a39fc62b0a7cf958d91a1b8283f7e9b1428d7249
TaguchiRei <ren.len2426@gmail.com>
2025-06-18 17:27:32 +0900

    [add]保存ロジック完成

--- Assets/Scripts/Player/UnitAttachiDetaching.cs
+++ Assets/Scripts/Player/UnitAttachiDetaching.cs
@@ -1,8 +1,9 @@
-using System;
 using DIContainer;
+using GamesKeystoneFramework.KeyDebug.KeyLog;
 using Manager;
 using StaticObject;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
@@ -11,20 +12,32 @@ namespace Player
         private PlayerOperationManager _playerOperationManager;
         private UnitResourceManager _unitResourceManager;
         private GridManager _gridManager;
+
         private void Start()
         {
             DiContainer.Instance.TryGet(out _playerOperationManager);
             DiContainer.Instance.TryGet(out _unitResourceManager);
             DiContainer.Instance.TryGet(out _gridManager);
+            _playerOperationManager.OnInteractAction += OnInteract;
         }
 
-        private void OnInteract()
+        private void OnInteract(InputAction.CallbackContext context)
         {
+            KeyLogger.Log("OnInteract");
+            if (!context.started) return;
             //オブジェクトを設置する処理を書く
-            var data = _unitResourceManager.GetUnitData(UnitPutSupport.SelectedUnitType,UnitPutSupport.SelectedUnitID);
+            var data = _unitResourceManager.GetUnitData(UnitPutSupport.SelectedUnitType,
+                UnitPutSupport.SelectedUnitID);
             if (_gridManager.CheckCanPutUnit(data.UnitShape, UnitPutSupport.SelectedPosition))
             {
-                //_gridManager.
+                ulong shape = UnitPutSupport.SelectedUnitRotate switch
+                {
+                    GridManager.UnitRotate.Right90 => BitShapeSupporter.RotateRightUlongBase90(data.UnitShape),
+                    GridManager.UnitRotate.Right180 => BitShapeSupporter.RotateRightUlongBase180(data.UnitShape),
+                    GridManager.UnitRotate.Right270 => BitShapeSupporter.RotateRightUlongBase270(data.UnitShape),
+                    _ => data.UnitShape
+                };
+                _gridManager.PutUnitOnGrid(shape, UnitPutSupport.SelectedPosition);
             }
         }
     }
