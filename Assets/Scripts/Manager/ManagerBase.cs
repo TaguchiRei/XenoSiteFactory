4268375603cbc765db27d54a2341f4eb7deaa6cc
reitaghchi <ren.len2426@gmail.com>
2025-07-06 14:41:40 +0900

    [update]サマリーを追加

--- Assets/Scripts/Manager/ManagerBase.cs
+++ Assets/Scripts/Manager/ManagerBase.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace Manager
 {
+    /// <summary>
+    /// マネージャークラスのベースクラス。
+    /// Awakeでサービスロケーターに登録する
+    /// </summary>
     public abstract class ManagerBase : MonoBehaviour, IServiceRegistrable
     {
         public void Register()
@@ -16,6 +20,9 @@ namespace Manager
             Register();
         }
 
+        /// <summary>
+        /// 変数の初期化などを行う
+        /// </summary>
         public abstract void Initialize();
     }
 }
