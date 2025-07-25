47db5b6a10102ce9babb1eb8267ac6333bfb685b
reitaghchi <ren.len2426@gmail.com>
2025-07-25 16:45:20 +0900

    [add]取得用のメソッドをインターフェースに追加

--- Assets/Code/Scripts/Interface/ILayer.cs
+++ Assets/Code/Scripts/Interface/ILayer.cs
@@ -1,3 +1,4 @@
+using Service;
 using UnityEngine;
 
 namespace Interface
@@ -5,15 +6,26 @@ namespace Interface
     public interface IPresentationLayer
     {
         void RegisterPresentation();
+
+        bool GetDomain<T>(out T instance) where T : IDomainLayer
+        {
+            return LayeredServiceLocator.Instance.TryGetDomainLayer(out instance);
+        }
     }
 
     public interface IDomainLayer
     {
         void RegisterDomain();
+
+        bool GetData<T>(out T instance) where T : IDataLayer
+        {
+            return LayeredServiceLocator.Instance.TryGetDataLayer(out instance);
+        }
     }
 
     public interface IDataLayer
     {
         void RegisterData();
     }
+
 }
