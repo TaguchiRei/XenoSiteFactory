d16c56f03dcebb8afe5e77b7850b4605819c9147
reitaghchi <ren.len2426@gmail.com>
2025-07-25 15:10:00 +0900

    [add]インターフェースを保存するための変数を追加

--- Assets/Code/Scripts/Manager/TurnManager.cs
+++ Assets/Code/Scripts/Manager/TurnManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Interface;
 using Manager;
 
@@ -7,29 +8,33 @@ namespace TurnSystem
     {
         public bool IsPaused { get; set; }
 
+        private List<ITurnStartHandler> _turnStartHandlers;
+        private List<ITurnUpdateHandler> _turnUpdateHandlers;
+        private List<ITurnEndHandler> _turnEndHandlers;
+        
         private int _turnCount;
 
         public void Pause()
         {
-            
+            IsPaused = true;
         }
 
         public void Resume()
         {
-            
+            IsPaused = false;
         }
-
+        
+        public override void Initialize()
+        {
+            _turnCount = 0;
+        }
+        
         /// <summary>
         /// 次のターンを再生するメソッド
         /// </summary>
         private void NextTurn()
         {
             _turnCount++;
-        }
-
-        public override void Initialize()
-        {
-            _turnCount = 0;
         }
     }
 }
