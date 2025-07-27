87052bfc556f6317a18d64694a12a65786279f2b
reitaghchi <ren.len2426@gmail.com>
2025-07-27 15:45:17 +0900

    [add]マンハッタン距離を求めて登録するプログラムを追加

--- Assets/Code/Scripts/GridSystem/GridDistanceData.cs
+++ Assets/Code/Scripts/GridSystem/GridDistanceData.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using GamesKeystoneFramework.KeyMathBit;
 using Interface;
 using Service;
+using StaticObject;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace GridSystem
@@ -11,13 +15,15 @@ namespace GridSystem
     {
         private const int GRID_WIDTH = 128;
         private const int GRID_HEIGHT = 4;
-        
-        private byte[,,] _grid;
 
-        private Vector2Int[] _checkPositions;
-        
-        GridDistanceData()
+        private readonly int _manhattanDistance;
+        private readonly List<Vector3Int> _checkPositionOffset;
+        private readonly byte[,,] _grid;
+
+        GridDistanceData(int manhattanDistance)
         {
+            _manhattanDistance = manhattanDistance;
+            _checkPositionOffset = new();
             _grid = new byte[GRID_WIDTH, GRID_HEIGHT, GRID_WIDTH];
             int centerX = GRID_WIDTH / 2;
             int centerZ = GRID_WIDTH / 2;
@@ -33,16 +39,48 @@ namespace GridSystem
                     }
                 }
             }
+
+            for (int x = 0; x < _manhattanDistance; x++)
+            {
+                for (int y = 0; y < _manhattanDistance; y++)
+                {
+                    if (x + y > _manhattanDistance) continue;
+                    _checkPositionOffset.Add(new(x, 0, y));
+                }
+            }
         }
 
         /// <summary>
         /// 設置ユニットの距離データを保存する
         /// </summary>
         /// <param name="shape"></param>
-        /// <param name="position"></param>
-        public void SetGridData(ulong shape, Vector3Int position)
+        /// <param name="setPosition"></param>
+        public void SetGridData(ulong shape, Vector3Int setPosition)
         {
-            
+            int edge = BitShapeSupporter.GetEdge();
+            DUlong oneDUlong = new DUlong(0, 1);
+            for (int x = 0; x < edge; x++)
+            {
+                for (int y = 0; y < edge; y++)
+                {
+                    for (int z = 0; z < edge; z++)
+                    {
+                        int bitPosition = BitShapeSupporter.CalculationBitPosition(x, y, z);
+                        if ((shape & (1ul << bitPosition)) == 0) continue;
+                        Vector3Int newPos = new(setPosition.x + x, setPosition.y + y, setPosition.z + z);
+                        foreach (var checkPositionOffset in _checkPositionOffset)
+                        {
+                            Vector3Int checkPosition = newPos + checkPositionOffset;
+                            Vector3Int checkVector = newPos - checkPosition;
+                            byte manhattan = (byte)(Mathf.Abs(checkVector.x) + Mathf.Abs(checkVector.z));
+                            if (_grid[checkPosition.x, checkPosition.y, checkPosition.z] > manhattan)
+                            {
+                                _grid[checkPosition.x, checkPosition.y, checkPosition.z] = manhattan;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         public void Dispose()
