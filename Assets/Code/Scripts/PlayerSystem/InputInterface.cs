144ab8d17a2678307a40ac7434a90220357f6ba0
reitaghchi <ren.len2426@gmail.com>
2025-08-29 15:18:40 +0900

    [add]入力を解釈するためのインターフェース作成

---
+++ Assets/Code/Scripts/PlayerSystem/InputInterface.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PlayerSystem
+{
+    public interface IButtonInputReceiver
+    {
+        public void OnButtonInput(InputAction.CallbackContext context, ButtonKinds buttonKind);
+    }
+
+    public interface ISwipeInputReceiver
+    {
+        public void OnSwipeInput(InputAction.CallbackContext context);
+    }
+
+    public interface ITapInputReceiver
+    {
+        public void OnTapInput(InputAction.CallbackContext context);
+    }
+
+    public enum ButtonKinds
+    {
+        MenuButton,
+        
+    }
+}
