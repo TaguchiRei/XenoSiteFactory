f6df5496e11441f0c72708c9b386b4c1f390a331
reitaghchi <ren.len2426@gmail.com>
2025-08-31 18:45:33 +0900

    [add]アウトゲームマネージャー作成開始

---
+++ Assets/Code/Scripts/OutGame/OutGameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace OutGame
+{
+    public class OutGameManager : MonoBehaviour
+    {
+        public void ShowAllSaveData()
+        {
+            
+        }
+
+        private void FindAllSaveData()
+        {
+            
+        }
+    }
+}
