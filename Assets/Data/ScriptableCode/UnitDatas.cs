e214de559fdc86ebbd78f88ea1453475dc08b79c
reitaghchi <ren.len2426@gmail.com>
2025-12-01 17:20:58 +0900

    [update]ユニットを保存するScriptableObject作成

--- Assets/Data/ScriptableCode/UnitDatas.cs
+++ Assets/Data/ScriptableCode/UnitDatas.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "UnitData", menuName = "ScriptableObjects/UnitData")]
 public class UnitDatas : ScriptableObject
 {
-    
+    public List<UnitData> AllUnitData = new();
 }
 
+[Serializable]
 public class UnitData
 {
-    
+    public string Name;
+    public string Info;
+    public Texture2D UnitTexture;
+    public GameObject UnitPrefab;
+    public Vector3Int[] InputPorts;
+    public Vector3Int[] OutputPorts;
+    public bool[] UnitShape;
+    public int UnitWidth;
+    public int UniDepth;
+    public int UnitHeight;
 }
