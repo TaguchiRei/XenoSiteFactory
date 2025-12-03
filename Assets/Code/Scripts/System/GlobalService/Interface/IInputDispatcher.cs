e0e3dfb8c8dcec468a6e7854eec87f1d3056855a
reitaghchi <ren.len2426@gmail.com>
2025-12-03 16:05:31 +0900

    [update]stringでアクションマップなどを受け取れるようにして違うMapのActionに登録しようとしたときの例外を出せるように

--- Assets/Code/Scripts/System/GlobalService/Interface/IInputDispatcher.cs
+++ Assets/Code/Scripts/System/GlobalService/Interface/IInputDispatcher.cs
@@ -6,27 +6,28 @@ public interface IInputDispatcher
     /// <summary>
     /// Actionの登録を行う
     /// </summary>
-    /// <param name="actionMap">ActionMapsをintにパースする</param>
-    /// <param name="actionNum">ActionMap + Actions　のEnumをintにパースして使う</param>
+    /// <param name="actionMap">ActionMapsをstringにパースする</param>
+    /// <param name="actionName">ActionMap + Actions　のEnumをstringにパースして使う</param>
     /// <param name="action"></param>
-    public void RegisterAction(int actionMap, int actionNum, Action<InputAction.CallbackContext> action);
+    public void RegisterAction(string actionMap, string actionName, Action<InputAction.CallbackContext> action);
 
     /// <summary>
     /// Actionの登録解除を行う
     /// </summary>
-    /// <param name="actionMap">ActionMapsをintにパースする</param>
-    /// <param name="actionNum">ActionMap + Actions　のEnumをintにパースして使う</param>
+    /// <param name="actionMap">ActionMapsをstringにパースする</param>
+    /// <param name="actionName">ActionMap + Actions　のEnumをstringにパースして使う</param>
     /// <param name="action"></param>
-    public void UnRegisterAction(int actionMap, int actionNum, Action<InputAction.CallbackContext> action);
+    public void UnRegisterAction(string actionMap, string actionName, Action<InputAction.CallbackContext> action);
 
     /// <summary>
     /// ActionMapを変える
     /// </summary>
-    /// <param name="actionMap"></param>
-    public void SwitchActionMap(int actionMap);
-
+    /// <param name="actionMap">ActionMapsをstringにパースする</param>
+    public void SwitchActionMap(string actionMap);
+    
     /// <summary>
-    /// 現在有効なActionMapを取得する
+    /// 現在有効なActionMapのIDを取得する
     /// </summary>
+    /// <returns>ActionMapにパースする</returns>
     public int GetActiveActionMap();
 }
